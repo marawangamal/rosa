df7848dedba8927892cf4fb3a70c5fb08e490de3
marawan gamal <marawan.gamal@live.com>
2023-08-15 23:27:06 -0400

    Apply singular values in one location only

--- factorizednet/factorized_module/rosa_linear.py
+++ factorizednet/factorized_module/rosa_linear.py
@@ -63,12 +63,16 @@ class RosaLinear(FactorizedLayer):
         u, s, vt = torch.linalg.svd(w, full_matrices=False)  # [out_f, r],[r,],[r, in_f] or [in_f, r],[r,],[r, out_f]
 
         if fan_in_fan_out:  # Conv1D
-            a = torch.sqrt(s).reshape(1, -1) * u  # [in_f, r]
-            b = torch.sqrt(s).reshape(-1, 1) * vt  # [r, out_f]
+            # a = torch.sqrt(s).reshape(1, -1) * u  # [in_f, r]
+            # b = torch.sqrt(s).reshape(-1, 1) * vt  # [r, out_f]
+            a = s.reshape(1, -1) * u
+            b = vt
             w_hat = a @ b
         else:
-            b = (torch.sqrt(s).reshape(1, -1) * u).T  # [out_f, r] => [r, out_f]
-            a = (torch.sqrt(s).reshape(-1, 1) * vt).T  # [r, in_f] => [in_f, r]
+            # b = (torch.sqrt(s).reshape(1, -1) * u).T  # [out_f, r] => [r, out_f]
+            # a = (torch.sqrt(s).reshape(-1, 1) * vt).T  # [r, in_f] => [in_f, r]
+            a = s.reshape(1, -1) * u
+            b = vt
             w_hat = b @ a
 
         # Check reconstruction error
