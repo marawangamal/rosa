f94c84e8141e6fd50f23428d9509017e319a9a20
Marawan Gamal <marawangamal@Marawans-MBP-2.lan>
2023-11-03 10:02:34 -0400

    Add more layers to aet

--- experiments/aet.py
+++ experiments/aet.py
@@ -10,7 +10,7 @@ class AETLinear(nn.Module):
         super().__init__(*args, **kwargs)
         self.a_trainable = aet
         self.w = nn.Parameter(torch.randn(in_features, out_features), requires_grad=False)
-        self.a = torch.randn(in_features, rank)
+        self.a = torch.randn(in_features, rank, requires_grad=False)
         self.b = nn.Parameter(torch.randn(rank, out_features), requires_grad=True)
         # if not self.b_trainable else nn.Parameter(torch.randn(rank, out_features), requires_grad=self.b_trainable)
 
@@ -36,32 +36,37 @@ class AETLinear(nn.Module):
         self.b = self.b.to(x.device)
         return x @ self.w + (x @ self.a) @ self.b
 
+
 class Model(nn.Module):
     def __init__(self,  in_features=10, hidden_features=10, out_features=10, rank=1, aet=True):
         super().__init__()
-        self.l1 = AETLinear(aet=aet, in_features=in_features, out_features=hidden_features, rank=rank)
-        self.l2 = AETLinear(aet=aet, in_features=hidden_features, out_features=out_features, rank=rank)
+        self.layers = nn.Sequential(
+            AETLinear(aet=aet, in_features=in_features, out_features=hidden_features, rank=rank),
+            AETLinear(aet=aet, in_features=hidden_features, out_features=hidden_features, rank=rank),
+            AETLinear(aet=aet, in_features=hidden_features, out_features=hidden_features, rank=rank),
+            AETLinear(aet=aet, in_features=hidden_features, out_features=hidden_features, rank=rank),
+            AETLinear(aet=aet, in_features=hidden_features, out_features=out_features, rank=rank)
+        )
 
     def forward(self, x):
-        rlu = torch.nn.functional.relu
-        return self.l2(rlu(self.l1(x)))
+        return self.layers(x)
 
 
 def report_memory_usage(message="", width=30):
     allocated = torch.cuda.memory_allocated() / (1024 ** 2)  # in MBs
     reserved = torch.cuda.memory_reserved() / (1024 ** 2)  # in MBs
-    print(f"{message.ljust(width)} --> Allocated memory: {allocated:.5f}MB, Reserved memory: {reserved:.5f}MB")
+    print(f"{message.ljust(width)} --> Allocated memory: {allocated:.10f}MB, Reserved memory: {reserved:.10f}MB")
 
 
 def main():
     # Define parameters
     params = {
         "batch_size": 512*32,
-        "in_features": 1024,
-        "out_features": 32,
+        "in_features": 512,
+        "out_features": 512,
         "rank": 1,
         "aet": False,
-        "hidden_features": 100,
+        "hidden_features": 512,
         "device": "cuda:0"
     }
 
@@ -73,26 +78,32 @@ def main():
 
     device = torch.device(params["device"])
 
-    report_memory_usage("Initial memory")
+    # report_memory_usage("Initial memory")
+    # w1 = torch.randn(params["in_features"], params["in_features"], device=device, requires_grad=False)
+    # a1 = torch.randn(params["in_features"], params["rank"], device=device, requires_grad=False)
+    # b1 = torch.randn(params["rank"], params["in_features"], device=device, requires_grad=False)
+    # w2 = torch.randn(params["in_features"], params["out_features"], device=device, requires_grad=False)
+    # a2 = torch.randn(params["in_features"], params["rank"], device=device, requires_grad=True)
+    # b2 = torch.randn(params["rank"], params["out_features"], device=device, requires_grad=False)
+    report_memory_usage("After model creation on GPU")
 
+    # Model
     model = Model(in_features=params["in_features"], hidden_features=params["hidden_features"], out_features=params["out_features"], rank=params["rank"], aet=params["aet"])
-    report_memory_usage("After model creation")
-
-    print(model)
-
-    x_true = torch.randn(params["batch_size"], params["in_features"], device=device)
-    y_true = torch.randn(params["batch_size"], params["out_features"], device=device)
-    report_memory_usage("After data creation")
-
-    # Move to GPU
     model.to(device)
-    report_memory_usage("After moving model to GPU")
+    report_memory_usage("After model creation on GPU")
+
+    x_true = torch.randn(params["batch_size"], params["in_features"], device=device, requires_grad=False)
+    y_true = torch.randn(params["batch_size"], params["out_features"], device=device, requires_grad=False)
+    report_memory_usage("After data creation")
 
     # Forward pass
     loss = torch.nn.functional.mse_loss(model(x_true), y_true)
+    # x2 = x_true @ w1 + (x_true @ a1) @ b1
+    # loss = torch.nn.functional.mse_loss(x2 @ w2 + (x2 @ a2) @ b2, y_true)
     print(f"Loss: {loss}")
-
     report_memory_usage("After forward pass")
+    # loss.backward()
+    # report_memory_usage("After backward pass")
 
 if __name__ == "__main__":
     main()
